e4523a39902f8c4a46510869b7e8bc2ca1ce0ce2
southoftheriver <anwjsrlrhwkd@naver.com>
2023-05-21 01:48:26 +0900

    feat: spring security, jwt, kakao oauth2

---
+++ src/main/java/jy/demo/config/SecurityConfig.java
@@ -0,0 +1,104 @@
+package jy.demo.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import jy.demo.security.jwt.filter.FilterSkipMatcher;
+import jy.demo.security.jwt.filter.HeaderTokenExtractor;
+import jy.demo.security.jwt.filter.JwtAuthFilter;
+import jy.demo.security.jwt.provider.JwtAuthentication;
+import jy.demo.security.oauth2.CustomAuthenticationFailureHandler;
+import jy.demo.security.oauth2.CustomAuthenticationSuccessHandler;
+import jy.demo.security.oauth2.CustomOAuth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtAuthentication jwtAuthentication;
+    private final HeaderTokenExtractor headerTokenExtractor;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    private final CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
+
+    @Autowired
+    public SecurityConfig(JwtAuthentication jwtAuthentication,
+        HeaderTokenExtractor headerTokenExtractor,
+        CustomOAuth2UserService customOAuth2UserService,
+        CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler,
+        CustomAuthenticationFailureHandler customAuthenticationFailureHandler) {
+        this.jwtAuthentication = jwtAuthentication;
+        this.headerTokenExtractor = headerTokenExtractor;
+        this.customOAuth2UserService = customOAuth2UserService;
+        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
+        this.customAuthenticationFailureHandler = customAuthenticationFailureHandler;
+    }
+
+
+    @Override
+    public void configure(WebSecurity web){
+        web
+            .ignoring()
+            .antMatchers("/h2-console/**");
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(jwtAuthentication.getProvider());
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+
+        http
+            .csrf().disable()
+//            .cors().configurationSource(corsConfigurationSource())
+//            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        http
+            .oauth2Login()
+            .defaultSuccessUrl("/main")
+            .successHandler(customAuthenticationSuccessHandler)
+            .failureHandler(customAuthenticationFailureHandler)
+            .userInfoEndpoint()
+            .userService(customOAuth2UserService);
+    }
+
+
+    private JwtAuthFilter jwtFilter() throws Exception {
+        List<String> skipPathList = new ArrayList<>();
+
+        // 회원 관리 API 허용
+        skipPathList.add("POST,/signup/**");
+        skipPathList.add("POST,/login/**");
+        skipPathList.add("GET,/login/**");
+        skipPathList.add("GET,/main");
+        skipPathList.add("GET,/test");
+
+        FilterSkipMatcher matcher = new FilterSkipMatcher(
+            skipPathList,
+            "/**"
+        );
+
+        JwtAuthFilter filter = new JwtAuthFilter(
+            matcher,
+            headerTokenExtractor
+        );
+        filter.setAuthenticationManager(super.authenticationManagerBean());
+
+        return filter;
+    }
+
+}
---
+++ src/main/java/jy/demo/security/oauth2/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,47 @@
+package jy.demo.security.oauth2;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import jy.demo.security.jwt.provider.JwtAuthenticationImpl;
+import jy.demo.security.jwt.provider.JwtTokenUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Slf4j
+@Component
+public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Value("${frontend.url}")
+    private String FRONTEND_URL;
+    private final JwtAuthenticationImpl jwtAuthentication;
+
+    public CustomAuthenticationSuccessHandler(JwtAuthenticationImpl jwtAuthentication) {
+        this.jwtAuthentication = jwtAuthentication;
+    }
+
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+        Authentication authentication) throws IOException, ServletException {
+
+        log.info("social login success");
+
+        String jwt = jwtAuthentication.generateToken((CustomOAuth2User) authentication.getPrincipal());
+
+        Cookie cookie = new Cookie("Authorization", jwt);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
+
+        response.sendRedirect(FRONTEND_URL + ":8080");
+
+    }
+
+}
